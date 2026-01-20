f182cb27fcfb331306b9bcdc4701c33bf02f1783
KevinLatino <latinokevin9@gmail.com>
2026-01-19 19:35:12 -0600

    feature(products): adding products section

---
+++ src/components/visuals/WaveLeft.tsx
@@ -0,0 +1,24 @@
+'use client';
+
+import Image from 'next/image';
+
+interface WaveLeftProps {
+  className?: string;
+}
+
+export function WaveLeft({ className = '' }: WaveLeftProps) {
+  return (
+    <div className={`absolute bottom-0 left-0 z-0 ${className}`}>
+      <Image
+        src="/oppia-projects/wave-left.svg"
+        alt=""
+        width={400}
+        height={480}
+        className="h-[480px] w-[400px] opacity-80"
+        aria-hidden="true"
+        priority={false}
+      />
+    </div>
+  );
+}
+
